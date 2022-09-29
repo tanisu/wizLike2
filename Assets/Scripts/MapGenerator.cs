422c2ab032579ec103de3e253fb662f23d118a7c
tanisu <walk.away.renee.5125@gmail.com>
2022-09-29 15:47:08 +0900

    Initial commit

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField] TextAsset mapText;
+    [SerializeField] GameObject[] prefabs;
+
+    enum MAP_TYPE
+    {
+        GROUND, //0
+        WALL,   //1
+        PLAYER  //2
+    }
+    MAP_TYPE[,] mapTable;
+    float mapSize;
+    Vector2 centerPos;
+
+    void Start()
+    {
+        _loadMapData();
+        _createMap();
+    }
+
+    void _loadMapData()
+    {
+        string[] mapLines = mapText.text.Split(new[] { '\n', '\r' }, System.StringSplitOptions.RemoveEmptyEntries);
+
+
+        int row = mapLines.Length;
+        int col = mapLines[0].Split(new char[] { ',' }).Length;
+        mapTable = new MAP_TYPE[col,row];
+
+        for (int y = 0; y < row; y++)
+        {
+            string[] mapValues = mapLines[y].Split(new char[] { ',' });
+            for (int x = 0; x < col; x++)
+            {
+                mapTable[x, y] = (MAP_TYPE)int.Parse(mapValues[x]);
+            }
+        }
+    }
+
+    void _createMap()
+    {
+        mapSize = prefabs[1].GetComponent<SpriteRenderer>().bounds.size.x;
+        Debug.Log(mapSize);
+
+
+        //’†SÀ•W‚ğæ“¾
+        //c‰¡‚Ì”‚ğ”¼•ª‚É‚µ‚ÄmapSize‚ğŠ|‚¯‚é‚±‚Æ‚Å’†S‚ğ‹‚ß‚é
+        if(mapTable.GetLength(0) % 2 == 0)
+        {
+            centerPos.x = mapTable.GetLength(0) / 2 * mapSize - (mapSize / 2);
+        }
+        else
+        {
+            centerPos.x = mapTable.GetLength(0) / 2 * mapSize;
+        }
+        
+        if(mapTable.GetLength(1) % 2 == 0)
+        {
+            centerPos.y = mapTable.GetLength(1) / 2 * mapSize - ( mapSize / 2);
+        }
+        else
+        {
+            centerPos.y = mapTable.GetLength(1) / 2 * mapSize;
+        }
+        
+
+        for (int y = 0;y < mapTable.GetLength(1); y++)
+        {
+            for(int x = 0;x < mapTable.GetLength(0); x++)
+            {
+                Vector2Int pos = new Vector2Int(x, y);
+                GameObject _ground  = Instantiate(prefabs[(int)MAP_TYPE.GROUND]);
+                GameObject _map = Instantiate(prefabs[(int)mapTable[x,y]]);
+                _ground.transform.position = _screenPos(pos);
+                _map.transform.position = _screenPos(pos);
+            }
+        }
+    }
+
+    Vector2 _screenPos(Vector2Int _pos)
+    {
+        return new Vector2(
+            _pos.x * mapSize - centerPos.x ,
+            -(_pos.y * mapSize - centerPos.y));
+
+    }
+}
