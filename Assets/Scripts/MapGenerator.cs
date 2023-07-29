b7e98cc220e50302adfbbbb4e65a271982c26cdc
tanisu <walk.away.renee.5125@gmail.com>
2023-07-30 08:45:07 +0900

    ボタンコントロール
    ボタンコントロール

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,8 +7,11 @@ public class MapGenerator : MonoBehaviour
 {
     [SerializeField] TextAsset mapText;
     [SerializeField] GameObject[] prefabs;
-    
-
+    [SerializeField] Transform map2D;
+    [SerializeField] WallsArr[] wallsArr;
+    public GameObject buttons;
+    MazeCreator maze;
+    int[,] mazeDatas;
     public enum MAP_TYPE
     {
         GROUND, //0
@@ -18,33 +21,37 @@ public class MapGenerator : MonoBehaviour
     MAP_TYPE[,] mapTable;
     float mapSize;
     Vector2 centerPos;
+    public int w, h;
+
     
 
     void Start()
     {
-        
+        w = Random.Range(5,15);
+        h = Random.Range(5, 15);
+        if (w % 2 == 0) w++;
+        if (h % 2 == 0) h++;
         _loadMapData();
         _createMap();
-        
+        map2D.position = new Vector3(5f, 0);
     }
 
     void _loadMapData()
     {
-        string[] mapLines = mapText.text.Split(new[] { '\n', '\r' }, System.StringSplitOptions.RemoveEmptyEntries);
-
-
-        int row = mapLines.Length;
-        int col = mapLines[0].Split(new char[] { ',' }).Length;
-        mapTable = new MAP_TYPE[col,row];
-
-        for (int y = 0; y < row; y++)
+        maze = new MazeCreator(w, h);
+        mazeDatas = new int[w,h];
+        mazeDatas = maze.CreateMaze();
+        int row = mazeDatas.GetLength(1);
+        int col = mazeDatas.GetLength(0);
+        mapTable = new MAP_TYPE[col, row];
+        for(int y = 0;y < row; y++)
         {
-            string[] mapValues = mapLines[y].Split(new char[] { ',' });
-            for (int x = 0; x < col; x++)
+            for(int x = 0;x < col; x++)
             {
-                mapTable[x, y] = (MAP_TYPE)int.Parse(mapValues[x]);
+                mapTable[x, y] = (MAP_TYPE)mazeDatas[x, y];
             }
         }
+
     }
 
     void _createMap()
@@ -77,16 +84,21 @@ public class MapGenerator : MonoBehaviour
             for(int x = 0;x < mapTable.GetLength(0); x++)
             {
                 Vector2Int pos = new Vector2Int(x, y);
-                GameObject _ground  = Instantiate(prefabs[(int)MAP_TYPE.GROUND],transform);
-                GameObject _map = Instantiate(prefabs[(int)mapTable[x,y]],transform);
+                GameObject _ground  = Instantiate(prefabs[(int)MAP_TYPE.GROUND],map2D);
+                GameObject _map = Instantiate(prefabs[(int)mapTable[x,y]],map2D);
                 _ground.transform.position = ScreenPos(pos);
+                
                 _map.transform.position = ScreenPos(pos);
 
-                if(mapTable[x,y] == MAP_TYPE.PLAYER)
+                if (x == 1 && y == 1)
                 {
-                    _map.GetComponent<Player>().currentPos = pos;
-                    
+                    GameObject _player = Instantiate(prefabs[2], map2D);
+                    _player.transform.position = ScreenPos(pos);
+                    _player.GetComponent<Player>().currentPos = pos;
+                    _player.GetComponent<Player>().mapGenerator = this;
                 }
+                
+                
             }
         }
     }
@@ -94,13 +106,38 @@ public class MapGenerator : MonoBehaviour
     public Vector2 ScreenPos(Vector2Int _pos)
     {
         return new Vector2(
-            _pos.x * mapSize - centerPos.x ,
+            _pos.x * mapSize - centerPos.x  ,
             -(_pos.y * mapSize - centerPos.y));
-
     }
 
     public MAP_TYPE GetNextMapType(Vector2Int _pos)
     {
         return mapTable[_pos.x, _pos.y];
     }
+
+    public void View3D(int _idx)
+    {
+        foreach(GameObject wall in wallsArr[_idx].wall)
+        {
+            wall.SetActive(true);
+        }
+    }
+
+    public void ResetView3D()
+    {
+        foreach (WallsArr walls in wallsArr)
+        {
+            foreach(GameObject wall in walls.wall)
+            {
+                wall.SetActive(false);
+            }
+        }
+    }
+}
+
+
+[System.Serializable]
+public class WallsArr
+{
+    public GameObject[] wall;
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,57 +1,137 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     
     public enum DIRECTION
     {
-        TOP,
+        UP,
         RIGHT,
         DOWN,
-        LEFT
+        LEFT,
+        MAX
     }
 
     public DIRECTION direction;
     public Vector2Int currentPos, nextPos;
     int[,] move = { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
+    int[,,] locations = {
+           {{-1,0 },{1,0 },{0,0 } },
+           {{0,-1 },{0,1 },{0,0 } },
+           {{1,0 },{-1,0 },{0,0 } },
+           {{0,1 },{0,-1 },{0,0 } },
+    };
+
     
-    MapGenerator mapGenerator;
+    Button[] cursors;
+
+    [SerializeField] Transform directionArrow;
+    Vector3[] arrowPositions = new[] { new Vector3(0,0.35f), new Vector3(0.35f, 0), new Vector3(0f, -0.35f),new Vector3(-0.35f,0f) };
+
+    public MapGenerator mapGenerator;
 
     private void Start()
     {
-        mapGenerator = transform.parent.GetComponent<MapGenerator>();
-        
+
+        cursors = mapGenerator.buttons.GetComponentsInChildren<Button>();
+        cursors[(int)DIRECTION.UP].onClick.AddListener(() => MoveForward());
+        cursors[(int)DIRECTION.RIGHT].onClick.AddListener(() => TurnRight());
+        cursors[(int)DIRECTION.DOWN].onClick.AddListener(() => TurnBack());
+        cursors[(int)DIRECTION.LEFT].onClick.AddListener(() => TurnLeft());
+
         direction = DIRECTION.DOWN;
+        _viewArrow();
+        mapGenerator.ResetView3D();
+        _getMapPositions();
     }
 
 
+    void MoveForward()
+    {
+        mapGenerator.ResetView3D();
+        _move();
+        _getMapPositions();
+    }
 
+    void TurnRight()
+    {
+        mapGenerator.ResetView3D();
+        direction++;
+        _setDirection();
+        _viewArrow();
+        _getMapPositions();
+    }
 
+    void TurnBack()
+    {
+        mapGenerator.ResetView3D();
+        direction += 2;
+        _setDirection();
+        _viewArrow();
+        _getMapPositions();
+    }
+    void TurnLeft()
+    {
+        mapGenerator.ResetView3D();
+        direction--;
+        _setDirection();
+        _viewArrow();
+        _getMapPositions();
+    }
     
+
+
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            direction = DIRECTION.TOP;
+            mapGenerator.ResetView3D();
             _move();
+            _getMapPositions();
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            direction = DIRECTION.RIGHT;
-            _move();
+            mapGenerator.ResetView3D();
+            direction++;
+            _setDirection();
+            _viewArrow();
+            _getMapPositions();
+
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            direction = DIRECTION.DOWN;
-            _move();
+            mapGenerator.ResetView3D();
+            direction += 2;
+            _setDirection();
+            _viewArrow();
+            _getMapPositions();
+
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            direction = DIRECTION.LEFT;
-            _move();
+            mapGenerator.ResetView3D();
+            direction--;
+            _setDirection();
+            _viewArrow();
+            _getMapPositions();
+
         }
+    }
+
+    void _setDirection()
+    {
+        int d = ((int)direction + (int)DIRECTION.MAX) % (int)DIRECTION.MAX;
+        direction = (DIRECTION)d;
+        
+    }
+
+    void _viewArrow()
+    {
+        directionArrow.localPosition = arrowPositions[(int)direction];
     }
     
 
@@ -64,8 +144,29 @@ public class Player : MonoBehaviour
             currentPos = nextPos;
         }
     }
-    
 
+    void _getMapPositions()
+    {
+        int depth = 3;
+        int width = 2;
+        
+        for (int d = depth; d >= 0; d--)
+        {
+            for (int w = 0; w <= width; w++)
+            {
+                int mapX = currentPos.x + locations[(int)direction, w, 0] + move[(int)direction, 0] * d;
+                int mapY = currentPos.y + locations[(int)direction, w, 1] + move[(int)direction, 1] * d;
+                if(mapX >= 0 && mapX < mapGenerator.w && mapY >=0 && mapY < mapGenerator.h)
+                {
+                    
+                    if (mapGenerator.GetNextMapType(new Vector2Int(mapX, mapY)) == MapGenerator.MAP_TYPE.WALL)
+                    {
+                        int idx = d * depth + width - w;
+                        mapGenerator.View3D(idx);
+                    }
 
-    
+                }
+            }
+        }
+    }
 }
